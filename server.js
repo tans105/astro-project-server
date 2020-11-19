bb3853af6eed97d40e29f73ffe51d57e688db840
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-11-19 19:43:20 +0530

    Added dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.options('*', cors());

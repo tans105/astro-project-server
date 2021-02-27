14bf9ed88c598c3a1a422b5339b64afdb501f0a6
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2021-02-27 21:01:52 +0530

    Feedback email integration

--- src/app/service/emailService.js
+++ src/app/service/emailService.js
@@ -1,122 +1,45 @@
 const nodemailer = require('nodemailer');
-
-function getTemplate(data) {
-    const email = data['email'],
-        primary = data['primary'],
-        secondary = data['secondary'],
-        latitude = data['latitude'],
-        longitude = data['longitude'],
-        dob = JSON.stringify(data['dob']),
-        pob = data['pob'],
-        tob = JSON.stringify(data['tob']),
-        fname = data['fname'],
-        questions = data['questions'];
-
-    let quesTpl = '';
-    questions.forEach((question) => {
-        quesTpl += `<li>${JSON.stringify(question)}</li>`
-    });
-
-    return `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <style>
-      table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-      }
-      
-      td, th {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-      }
-      
-      tr:nth-child(even) {
-        background-color: #dddddd;
-      }
-    </style>
-    </head>
-    <body>
-      
-      <h2>Query</h2>
-      <br>
-      
-      <table>
-        <tr>
-          <td>Full Name</td>
-          <td>${fname}</td>
-        </tr>
-        <tr>
-          <td>Email</td>
-          <td>${email}</td>
-        </tr>
-        <tr>
-          <td>Primary Contact</td>
-          <td>${primary}</td>
-        </tr>
-        <tr>
-          <td>Secondary Contact</td>
-          <td>${secondary}</td>
-        </tr>
-        <tr>
-          <td>Place of Birth</td>
-          <td>${pob}</td>
-        </tr>
-        <tr>
-          <td>Latitude</td>
-          <td>${latitude}</td>
-        </tr>
-        <tr>
-          <td>Longitude</td>
-          <td>${longitude}</td>
-        </tr>
-        <tr>
-          <td>Date of Birth</td>
-          <td>${dob}</td>
-        </tr>
-        <tr>
-          <td>Time of Birth</td>
-          <td>${tob}</td>
-        </tr>
-        <tr>
-          <td>Questions</td>
-          <td><ul>${quesTpl}</ul></td>
-        </tr>
-      </table>
-      
-    </body>
-    </html>
-    `;
-}
+const common = require('./common')
+const templateService = require('./templateService')
 
 module.exports = {
     sendEmail: function (req, res, cb) {
+        const config = common.config();
+        let emailConfig = {
+            email: ''
+        };
+
+        if (config) {
+            emailConfig = config.email;
+        } else {
+            cb({success: false, msg: 'Failed to send email: Configuration not found'}, res);
+            return;
+        }
+
         const transporter = nodemailer.createTransport({
-                service: 'yahoo',
+                service: emailConfig.service,
                 auth: {
-                    user: 'tanmayawasthi105@yahoo.com',
-                    pass: 'kictppfrqtlrfjwi'
+                    user: emailConfig.user,
+                    pass: emailConfig.pass
                 }
             }),
-            data = req.body,
-            template = getTemplate(data);
+            data = req.body;
+
+        const emailPayload = templateService.getTemplate(data);
 
         const mailOptions = {
-            from: 'tanmayawasthi105@yahoo.com',
-            to: 'sandhya_awasthi2005@yahoo.co.in',
-            cc: 'tanmayawasthi105@yahoo.com',
-            subject: `AstroPundit - New Query - ${data['fname']}`,
-            html: template
+            from: emailConfig.from,
+            to: emailConfig.to,
+            cc: (emailConfig.cc && emailConfig.cc.length > 0) ? emailConfig.cc[0] : null,
+            subject: emailPayload.subject,
+            html: emailPayload.body
         };
 
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
-                cb({success: false, msg: 'Failed to send email ' + error}, res);
+                cb({success: false, msg: 'Failed to send email: ' + error}, res);
             } else {
-                cb({success: true, msg: 'Updated successfully ' + info.message}, res);
+                cb({success: true, msg: 'Updated successfully: ' + info.message}, res);
             }
         });
     }
---
+++ src/app/service/templateService.js
@@ -0,0 +1,160 @@
+module.exports = {
+    getTemplate: (data) => {
+        const type = data['emailType'];
+        if (type) {
+            return (type === 'feedback') ? getFeedbackTemplate(data) : getQueryTemplate(data)
+        } else {
+            return null;
+        }
+    }
+}
+
+function getQueryTemplate(data) {
+    const email = data['email'],
+        primary = data['primary'],
+        secondary = data['secondary'],
+        latitude = data['latitude'],
+        longitude = data['longitude'],
+        dob = JSON.stringify(data['dob']),
+        pob = data['pob'],
+        tob = JSON.stringify(data['tob']),
+        fname = data['fname'],
+        questions = data['questions'];
+
+    let quesTpl = '';
+    questions.forEach((question) => {
+        quesTpl += `<li>${JSON.stringify(question)}</li>`
+    });
+
+    let subject = `AstroPundit - New Query - ${fname}`;
+    let body = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <style>
+      table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+      }
+      
+      td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+      }
+      
+      tr:nth-child(even) {
+        background-color: #dddddd;
+      }
+    </style>
+    </head>
+    <body>
+      
+      <h2>Query</h2>
+      <br>
+      
+      <table>
+        <tr>
+          <td>Full Name</td>
+          <td>${fname}</td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td>${email}</td>
+        </tr>
+        <tr>
+          <td>Primary Contact</td>
+          <td>${primary}</td>
+        </tr>
+        <tr>
+          <td>Secondary Contact</td>
+          <td>${secondary}</td>
+        </tr>
+        <tr>
+          <td>Place of Birth</td>
+          <td>${pob}</td>
+        </tr>
+        <tr>
+          <td>Latitude</td>
+          <td>${latitude}</td>
+        </tr>
+        <tr>
+          <td>Longitude</td>
+          <td>${longitude}</td>
+        </tr>
+        <tr>
+          <td>Date of Birth</td>
+          <td>${dob}</td>
+        </tr>
+        <tr>
+          <td>Time of Birth</td>
+          <td>${tob}</td>
+        </tr>
+        <tr>
+          <td>Questions</td>
+          <td><ul>${quesTpl}</ul></td>
+        </tr>
+      </table>
+      
+    </body>
+    </html>
+    `;
+
+    return {subject, body};
+}
+
+function getFeedbackTemplate(data) {
+    const email = data['email'],
+        name = data['name'],
+        feedback = data['feedback'];
+
+    let subject = 'Feedback Recieved !'
+    let body = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <style>
+      table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+      }
+      
+      td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+      }
+      
+      tr:nth-child(even) {
+        background-color: #dddddd;
+      }
+    </style>
+    </head>
+    <body>
+      
+      <h2>Query</h2>
+      <br>
+      
+      <table>
+        <tr>
+          <td>Full Name</td>
+          <td>${name}</td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td>${email}</td>
+        </tr>
+        <tr>
+          <td>Feedback</td>
+          <td>${feedback}</td>
+        </tr>
+      </table>
+      
+    </body>
+    </html>
+    `;
+
+    return {subject, body};
+}

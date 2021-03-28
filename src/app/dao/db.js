2c2342847ef5cac8d1a7f2d3cb33a30474d7daf0
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2021-03-28 20:00:57 +0530

    Database integration with heroku

--- src/app/dao/db.js
+++ src/app/dao/db.js
@@ -63,8 +63,10 @@ const makeConnection = () => {
         sequelize = new Sequelize(process.env.DATABASE_URL, {
             dialect: 'postgres',
             operatorsAliases: false,
-            ssl: {
-                rejectUnauthorized: false
+            dialectOptions: {
+                ssl: {
+                    rejectUnauthorized: false
+                }
             },
             pool: {
                 max: 5,

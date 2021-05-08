1210b0cd674e01623b1e93c83fd5ad0e5f72aa56
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2021-05-08 21:51:44 +0530

    Consumption of Kundli Girls details

--- src/app/service/template.service.js
+++ src/app/service/template.service.js
@@ -1,125 +1,187 @@
 const parseUtil = require('../utils/parse.util');
 
 module.exports = {
-    getTemplate: (data) => {
-        const type = data['emailType'];
-        if (type) {
-            return (type === 'feedback') ? getFeedbackTemplate(data) : getQueryTemplate(data)
-        } else {
-            return null;
-        }
+  getTemplate: (data) => {
+    const type = data['emailType'];
+    if (type) {
+      return (type === 'feedback') ? getFeedbackTemplate(data) : getQueryTemplate(data)
+    } else {
+      return null;
     }
+  }
 }
 
 function getQueryTemplate(data) {
-    const {
-        email,
-        primary,
-        secondary,
-        sob,
-        dob,
-        pob,
-        tob,
-        fname,
-        questions,
-        amount,
-        service
-    } = parseUtil.parse(data);
+  const {
+    email,
+    primary,
+    secondary,
+    sob,
+    dob,
+    pob,
+    tob,
+    fname,
+    questions,
+    amount,
+    service,
+    sob_girl,
+    dob_girl,
+    pob_girl,
+    tob_girl,
+    fname_girl,
+  } = parseUtil.parse(data);
 
-    let quesTpl = '';
-    questions.forEach((question) => {
-        quesTpl += `<li>${JSON.stringify(question)}</li>`
-    });
+  let quesTpl = '';
+  questions.forEach((question) => {
+    quesTpl += `<li>${JSON.stringify(question)}</li>`
+  });
 
-    let subject = `MangalamBhav - New Query - ${fname}`;
-    let body = `
+  let subject = `MangalamBhav - New Query - ${service} - ${email} - ${primary}`;
+  let body = `
     <!DOCTYPE html>
     <html>
-    <head>
-      <style>
-      table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-      }
-      
-      td, th {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-      }
-      
-      tr:nth-child(even) {
-        background-color: #dddddd;
-      }
-    </style>
-    </head>
-    <body>
-      
-      <h2>Query</h2>
-      <br>
-      
-      <table>
-        <tr>
-          <td>Service</td>
-          <td>${service}</td>
-        </tr>
-        <tr>
-          <td>Full Name</td>
-          <td>${fname}</td>
-        </tr>
-        <tr>
-          <td>Email</td>
-          <td>${email}</td>
-        </tr>
-        <tr>
-          <td>Primary Contact</td>
-          <td>${primary}</td>
-        </tr>
-        <tr>
-          <td>Secondary Contact</td>
-          <td>${secondary}</td>
-        </tr>
-        <tr>
-          <td>City of Birth</td>
-          <td>${pob}</td>
-        </tr>
-        <tr>
-          <td>State of Birth</td>
-          <td>${sob}</td>
-        </tr>
-        <tr>
-          <td>Date of Birth</td>
-          <td>${dob}</td>
-        </tr>
-        <tr>
-          <td>Time of Birth</td>
-          <td>${tob}</td>
-        </tr>
-        <tr>
-          <td>Questions</td>
-          <td><ul>${quesTpl}</ul></td>
-        </tr>
-        <tr>
-          <td>Amount</td>
-          <td>${amount}</td>
-        </tr>
-      </table>
-      
-    </body>
-    </html>
+       <head>
+          <style>
+             table {
+               font-family: arial, sans-serif;
+               border-collapse: collapse;
+               width: 100%;
+               margin: 20px 0;
+             }
+    
+             td, th {
+               border: 1px solid #dddddd;
+               text-align: left;
+               padding: 8px;
+               width: 50%
+             }
+    
+             .data-table tr {
+               background-color: #dddddd;
+               border-top: 2px solid white;
+               border-bottom: 2px solid white;
+             }
+    
+             .data-table td {
+               border: 2px solid white;
+               padding: 8px;
+             }
+    
+             ul {
+               padding: 0;
+               margin: 0;
+             }
+          </style>
+       </head>
+       <body>
+          <h2>Query</h2>
+          <br>
+          <table>
+             <tr>
+                <td>Service</td>
+                <td>${service}</td>
+             </tr>
+             <tr>
+                <td>Amount</td>
+                <td>${amount}</td>
+             </tr>
+          </table>
+          <table class="data-table">
+             <tr>
+                <td>Email</td>
+                <td>${email}</td>
+             </tr>
+             <tr>
+                <td>Primary Contact</td>
+                <td>${primary}</td>
+             </tr>
+             <tr>
+                <td>Secondary Contact</td>
+                <td>${secondary}</td>
+             </tr>
+          </table>
+          
+          <h3>Person 1 Details</h3>
+          <table class="data-table">
+            <tr>
+                <td>Full Name</td>
+                <td>${fname}</td>
+             </tr>
+             <tr>
+                <td>City of Birth</td>
+                <td>${pob}</td>
+             </tr>
+             <tr>
+                <td>State of Birth</td>
+                <td>${sob}</td>
+             </tr>
+             <tr>
+                <td>Date of Birth</td>
+                <td>${dob}</td>
+             </tr>
+             <tr>
+                <td>Time of Birth</td>
+                <td>${tob}</td>
+             </tr>
+          </table>
     `;
 
-    return {subject, body};
+  if (service === 'kundli') {
+    body += `
+      <h3>Person 2 Details</h3>
+      <table class="data-table">
+        <tr>
+            <td>Full Name</td>
+            <td>${fname_girl}</td>
+         </tr>
+         <tr>
+            <td>City of Birth</td>
+            <td>${pob_girl}</td>
+         </tr>
+         <tr>
+            <td>State of Birth</td>
+            <td>${sob_girl}</td>
+         </tr>
+         <tr>
+            <td>Date of Birth</td>
+            <td>${dob_girl}</td>
+         </tr>
+         <tr>
+            <td>Time of Birth</td>
+            <td>${tob_girl}</td>
+         </tr>
+      </table>   `
+  }
+
+  if (service === 'custom') {
+    body += `
+      <h3>Custom Query</h3>
+      <table class="data-table">
+         <tr>
+            <td>Questions</td>
+            <td>
+               <ul>${quesTpl}</ul>
+            </td>
+         </tr>
+      </table>
+        `
+  }
+
+  body += `
+    </body>
+    </html>
+    `
+
+  return {subject, body};
 }
 
 function getFeedbackTemplate(data) {
-    const email = data['email'],
-        name = data['name'],
-        feedback = data['feedback'];
+  const email = data['email'],
+    name = data['name'],
+    feedback = data['feedback'];
 
-    let subject = 'Feedback Recieved !'
-    let body = `
+  let subject = 'Feedback Recieved !'
+  let body = `
     <!DOCTYPE html>
     <html>
     <head>
@@ -165,5 +227,5 @@ function getFeedbackTemplate(data) {
     </html>
     `;
 
-    return {subject, body};
+  return {subject, body};
 }
--- src/app/utils/parse.util.js
+++ src/app/utils/parse.util.js
@@ -13,7 +13,12 @@ module.exports = {
                 fname: data['fname'],
                 questions: data['questions'],
                 amount: data['amount'],
-                service: data['service']
+                service: data['service'],
+                dob_girl: JSON.stringify(data['dob_girl']),
+                pob_girl: data['pob_girl'],
+                sob_girl: data['sob_girl'],
+                tob_girl: JSON.stringify(data['tob_girl']),
+                fname_girl: data['fname_girl'],
             }
             obj.success = true;
         } catch (e) {

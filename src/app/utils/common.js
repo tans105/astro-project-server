5bc55e16c276322929ba313586b0fa5de7ed486e
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2021-05-22 21:09:31 +0530

    Fixing heroku failure

--- src/app/utils/common.js
+++ src/app/utils/common.js
@@ -9,6 +9,9 @@ exports.config = () => {
 exports.setupConfiguration = () => {
     const runtime = process.env.ASTRO_NODE_ENV || 'development';
     setTimeout(()=> {
+        Logger.info('runtime-------------', runtime)
+        Logger.info('Env-----------', env)
+
         config = env[runtime];
         Logger.info('Runtime Configuration', config)
     }, 2000)
